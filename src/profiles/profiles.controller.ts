55e1e3859aa2299100e5d0d5e6cec07307bc275b
abdulahaddf <abdulahad.df1@gmail.com>
2026-01-22 22:59:07 +0600

    create profile dto for post

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+    //GET /profiles
+  @Get()
+  findAll(@Query('location') location: string) {
+    return [{location}];
+  }
+
+  //GET /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id : number) {
+    return {id};
+  }
+
+//POST /profiles
+@Post()
+create(@Body() createProfileDto: CreateProfileDto) {
+return{
+    name: createProfileDto.name,
+    description: createProfileDto.description,
+};
+
+};
+}
